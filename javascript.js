13d3ec10984e3c75566719810912e34c25843701
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-13 18:01:21 +0100

    Add operate function for handling calculator operations

--- javascript.js
+++ javascript.js
@@ -16,3 +16,20 @@ function divide(x, y) {
   }
   return x / y;
 }
+
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    default:
+      console.error(
+        "Error: Operate switch reached default state. Refresh the page and try again."
+      );
+  }
+}
