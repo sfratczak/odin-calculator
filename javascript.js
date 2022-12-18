aa9849a163585e26d0029a0c58e7902c703dd392
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-18 20:27:24 +0100

    Add notes and update AC, decimal, and equals buttons classes to ids

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const operators = document.querySelectorAll(".operator");
 let firstValue = 0;
 let secondValue = 0;
 let displayText = "0";
+let chosenOperator = "";
 
 updateDisplayText();
 
@@ -22,6 +23,20 @@ operands.forEach((operand) => {
 function updateDisplayText() {
   display.textContent = displayText;
 }
+
+/*
+1. user provides input for first value as text
+2. user clicks on an operator (+)
+3. current displayText is converted to firstValue
+4. save operator.textContent for operate() function
+4. current displayText remains visible until user provides new input for second value
+5. user provides input. displayText immediately updates to that input.
+6. user clicks on equals
+7. current displayText is converted to secondValue
+8. evaluate operate() using saved operator, firstValue, and secondValue
+9. replace displayText with above valuation and update displayText
+10. replace firstValue with valuation result
+*/
 
 function operate(operator, x, y) {
   switch (operator) {
