1d3856819af2e1191d1ed329b98b615cabda773f
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-18 20:08:26 +0100

    Add operand event listeners updating display text and display text updater

--- javascript.js
+++ javascript.js
@@ -1,3 +1,28 @@
+const display = document.querySelector("#display");
+const operands = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
+
+let firstValue = 0;
+let secondValue = 0;
+let displayText = "0";
+
+updateDisplayText();
+
+operands.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    if (displayText == "0") {
+      displayText = operand.textContent;
+    } else {
+      displayText += operand.textContent;
+    }
+    updateDisplayText();
+  });
+});
+
+function updateDisplayText() {
+  display.textContent = displayText;
+}
+
 function operate(operator, x, y) {
   switch (operator) {
     case "+":
