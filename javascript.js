2cc937f6c38c5aa65943956bebe9798de96fac95
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-18 19:45:26 +0100

    Integrate all four math functions directly into operate switch

--- javascript.js
+++ javascript.js
@@ -1,32 +1,16 @@
-function add(x, y) {
-  return x + y;
-}
-
-function subtract(x, y) {
-  return x - y;
-}
-
-function multiply(x, y) {
-  return x * y;
-}
-
-function divide(x, y) {
-  if (y === 0) {
-    return "NICETRY";
-  }
-  return x / y;
-}
-
 function operate(operator, x, y) {
   switch (operator) {
     case "+":
-      return add(x, y);
+      return x + y;
     case "-":
-      return subtract(x, y);
+      return x - y;
     case "*":
-      return multiply(x, y);
+      return x * y;
     case "/":
-      return divide(x, y);
+      if (y === 0) {
+        return "NICETRY";
+      }
+      return x / y;
     default:
       console.error(
         "Error: Operate switch reached default state. Refresh the page and try again."
